2d1b6ac1147b98b0a63cb643dd02dc0be6988d07
AnPLM <patricia_2897@hotmail.com>
2018-06-29 16:19:19 -0600

    Un poco de cliente

--- Restaurante/BL/Cliente.cs
+++ Restaurante/BL/Cliente.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TO;
-
 namespace BL
 {
     public class Cliente
     {
+        public String Nombre { get; set; }
+        public String Correo { get; set; }
+        public String NombreUsuario { get; set; }
+        public String Contrasenna { get; set; }
+        public Boolean Habilitado { get; set; }
+        public String Direccion { get; set; }
         public Cliente()
         {
         }
-        public Cliente(String nombre, String correo, String nombreUsuario, String contrasenna, int habilitado, String direccion)
+        public Cliente(String nombre, String correo, String nombreUsuario, String contrasenna, Boolean habilitado, String direccion)
         {
             this.Nombre = nombre;
             this.Correo = correo;
@@ -21,12 +26,15 @@ namespace BL
             this.Direccion = direccion;
         }
 
-        public String Nombre { get; set; }
-        public String Correo { get; set; }
-        public String NombreUsuario { get; set; }
-        public String Contrasenna { get; set; }
-        public int Habilitado { get; set; }
-        public String Direccion { get; set; }
+        
+
+        public void agregarCliente() {
+            
+        }
+        public void modificarCliente() { }
+        public void seleccionarCliente() { }
+        public void habilitarCliente() { }
+        public void deshabilitarCliente() { }
 
     }
 }
---
+++ Restaurante/DAO/DAOCliente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using TO;
+
+namespace DAO
+{
+    public class DAOCliente
+    {
+        SqlConnection connection = new SqlConnection(Properties.Settings.Default.Conn);
+
+        public void insertarCliente(TOCliente toCliente) {
+            try
+            {
+                SqlCommand command = new SqlCommand("insert into Cliente values (@nombre," +
+                "@correo, @nombreUsuario, @contr, @habilitado, @dir)", connection);
+                command.Parameters.AddWithValue("@nombre", toCliente.Nombre);
+                command.Parameters.AddWithValue("@correo", toCliente.Correo);
+                command.Parameters.AddWithValue("@nombreUsuario", toCliente.NombreUsuario);
+                command.Parameters.AddWithValue("@contr", toCliente.Contrasenna);
+                command.Parameters.AddWithValue("@habilitado", toCliente.Habilitado);
+                command.Parameters.AddWithValue("@dir", toCliente.Direccion);
+
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Ocurrió un error al insertar");
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }          
+        }
+        public void buscarCliente() { }
+        public void habilitarCliente() { }
+        public void deshabilitarCliente() { }
+    }
+}
