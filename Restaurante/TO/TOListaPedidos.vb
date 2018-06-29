c0ec53390866fdecb9dfd46643c0b553f72612cc
Seney Alvarado Carvajal <seney.alvarado@ucrso.info>
2018-06-29 14:07:25 -0600

    ListaPedido BL Y DAO v1.0

---
+++ Restaurante/TO/TOListaPedidos.vb
@@ -0,0 +1,32 @@
+﻿Public Class TOListaPedidos
+
+    Public Sub New(nombre_usuario As String, identificador_plato As Integer)
+        nombre = nombre_usuario
+        plato = identificador_plato
+    End Sub
+
+    Public Sub New()
+    End Sub
+
+    Private nombre As String
+    Public Property Nombre_Usuario() As String
+        Get
+            Return nombre
+        End Get
+        Set(ByVal value As String)
+            nombre = value
+        End Set
+    End Property
+
+    Private plato As String
+    Public Property Identificador_Plato() As Integer
+        Get
+            Return plato
+        End Get
+        Set(ByVal value As Integer)
+            plato = value
+        End Set
+    End Property
+
+
+End Class
