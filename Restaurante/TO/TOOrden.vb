8e139c5fa229a2cb358978224ea653d1a4d0e5dc
Adan Talavera <adan_talavera@hotmail.com>
2018-06-29 19:12:51 -0600

    hyujlo

---
+++ Restaurante/TO/TOOrden.vb
@@ -0,0 +1,18 @@
+﻿Public Class TOOrden
+
+    Public Property Nombre_Usuario As String
+    Public Property Fecha As DateTime
+    Public Property Estado As String
+    Public Property Identificador As Int32
+
+    Sub New()
+
+    End Sub
+
+    Sub New(Nombre As String, fecha As DateTime, Estado As String, identificador As Int32)
+        Me.Nombre_Usuario = Nombre
+        Me.Fecha = fecha
+        Me.Estado = Estado
+        Me.Identificador = identificador
+    End Sub
+End Class
