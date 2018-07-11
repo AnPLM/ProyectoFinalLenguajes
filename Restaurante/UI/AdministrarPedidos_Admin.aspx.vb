bf7e340ff0efd3b1b90174024ee930048bf6f4a5
AnPLM <patricia_2897@hotmail.com>
2018-07-10 20:08:49 -0600

    Parece que falto

---
+++ Restaurante/UI/AdministrarPedidos_Admin.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class AdministrarPedidos_Admin
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnConsultar_Click(sender As Object, e As EventArgs) Handles btnConsultar.Click
+        Dim cliente As String = txtCliente.Text.Trim()
+        Dim estado As String = txtEstado.Text.Trim()
+        Dim fechaInicio As String = txtFechaInicio.Text.Trim()
+        Dim fechaFin As String = txtFechaFin.Text.Trim()
+
+
+    End Sub
+End Class
